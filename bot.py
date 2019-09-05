6abd1ff93be6e7fd907e9f9e7be74f4b744afdcd
Alexander Rogachev <rogachev-a-v@yandex.ru>
2019-09-05 18:01:53 +0300

    try to add keyboard

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from vk import VK
 from vk.utils import TaskManager
 from vk.bot_framework import Dispatcher
+from vk.keyboards import Keyboard, ButtonColor
 from vk import types
 
 import logging
@@ -16,10 +17,16 @@ api = vk.get_api()
 
 dp = Dispatcher(vk, gid)
 
+keyboard = Keyboard(one_time=False)
+keyboard.add_text_button("Hi!", payload={"command": "hello"})
+keyboard.add_text_button("Bye:(", payload={"command": "bye"}, color=ButtonColor.SECONDARY)
 
-@dp.message_handler(commands=["help", "test", "start", "aoff"])
+@dp.message_handler(commands=["hello", "test", "start", "aoff"])
 async def handle(message: types.Message, data: dict):
-    await message.reply("Test!")
+    await message.reply("Hello!")
+@dp.message_handler(commands=["bye", "test", "start", "aoff"])
+async def handle(message: types.Message, data: dict):
+    await message.reply("Byeee!")
 
 
 @dp.message_handler(text="hello")
