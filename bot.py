00802fbf61d57ef4c7ea7417f1388383b54f7b45
Alexander Rogachev <rogachev-a-v@yandex.ru>
2019-09-06 12:52:26 +0300

    add some working buttons

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import logging
 
 from config import TOKEN, GROUP_ID#, PLUGINS_PATH #, loop
 
-logging.basicConfig(level="INFO")
+logging.basicConfig(level="DEBUG")
 
 vk = VK(TOKEN)
 gid = GROUP_ID
@@ -17,22 +17,47 @@ api = vk.get_api()
 
 dp = Dispatcher(vk, gid)
 
-keyboard = Keyboard(one_time=False)
-keyboard.add_text_button("Hi!", payload={"command": "hello"})
-keyboard.add_text_button("Bye:(", payload={"command": "bye"}, color=ButtonColor.SECONDARY)
+keyboard1 = Keyboard(one_time=False)
+keyboard1.add_text_button('Hi', payload={"command": 'hello'})
+keyboard1.add_text_button("Bye:(", payload={"command": 'bye'}, color=ButtonColor.SECONDARY)
 
-@dp.message_handler(commands=["hello", "test", "start", "aoff"])
-async def handle(message: types.Message, data: dict):
-    await message.reply("Hello!")
-@dp.message_handler(commands=["bye", "test", "start", "aoff"])
-async def handle(message: types.Message, data: dict):
-    await message.reply("Byeee!")
+keyboard_start = Keyboard(one_time=True)
+keyboard_start.add_text_button('Начинаем квест!', payload={"command": 'start'})
 
 
-@dp.message_handler(text="hello")
-async def handle_event(message: types.Message, data: dict):
-    await message.reply("Hello!")
+@dp.message_handler(payload={"command": 'start'})
+async def handle_start(message: types.Message, data: dict):
+    await message.reply("МОлодееец, начинаем работу с ботом! Получай клавиатуру", keyboard=keyboard1.get_keyboard())
 
+
+@dp.message_handler(text='hello')
+async def handle_text_start(message: types.Message, data: dict):
+    await message.reply("ты начал!")
+
+
+@dp.message_handler(payload={"command": 'hello'})
+async def handle_hello(message: types.Message, data: dict):
+    await message.reply("payload hello")
+
+
+#@dp.message_handler(payload=["bye", "test", "start", "aoff"])
+#async def handle(message: types.Message, data: dict):
+#    await message.reply("Bye!")
+
+
+@dp.message_handler(payload={"command": 'bye'})
+async def handle_bye(message: types.Message, data: dict):
+    await message.reply("bye payload", keyboard=keyboard.get_empty_keyboard())
+
+
+@dp.message_handler(text="off")
+async def off_keyboard(message: types.Message, data: dict):
+    await message.answer("Ok.", keyboard=keyboard_start.get_keyboard())
+
+
+@dp.message_handler()
+async def handle_other_messages(message: types.Message, data: dict):
+    await message.answer(message.text, keyboard=keyboard_start.get_keyboard() )#message.answer("another message", keyboard=keyboard.get_empty_keyboard())
 
 async def run():
     await dp.run_polling()
