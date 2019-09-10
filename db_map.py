b0727c3e0a1abfbb35ad2d613e0015f8d1920596
Alexander Rogachev <rogachev-a-v@yandex.ru>
2019-09-10 21:53:11 +0300

    fix comments

--- db_map.py
+++ db_map.py
@@ -15,29 +15,29 @@ class General(Base):  # Когда капитан вводит название 
     id = Column(Integer, primary_key=True)
     captain_id = Column(String(255))
     command_name = Column(String(255))
-    r1 = Column(Integer)  # mark for solving - 0 to 15 ISZ
+    r1 = Column(Integer)  # mark for solving - 0 to 15 ИСЗ
     k1 = Column(Integer)
-    r2 = Column(Integer)  # Prokhodnaya RKK
+    r2 = Column(Integer)  # проходная 1 РКК
     k2 = Column(Integer)
-    r3 = Column(Integer)  # Mozzhorin
+    r3 = Column(Integer)  # Мозжорин
     k3 = Column(Integer)
-    r4 = Column(Integer)  # Lunodrom
+    r4 = Column(Integer)  # лунодром
     k4 = Column(Integer)
-    r5 = Column(Integer)  # kolledzh dvigateli
+    r5 = Column(Integer)  # колледж двигатели
     k5 = Column(Integer)
-    r6 = Column(Integer)  # raketa u IPK Mashpribor
+    r6 = Column(Integer)  # ракета у ИПК МашПрибор
     k6 = Column(Integer)
-    r7 = Column(Integer)  # Besedka LUNA
+    r7 = Column(Integer)  # беседка Луна
     k7 = Column(Integer)
-    r8 = Column(Integer)  # pamyatnik korollevu na prospekte
+    r8 = Column(Integer)  # памятник Королёву на проспекте
     k8 = Column(Integer)
-    r9 = Column(Integer)  # pamyatnik Isaevu
+    r9 = Column(Integer)  # памятник Исаеву
     k9 = Column(Integer)
-    r10 = Column(Integer)  # Pushka Grabina
+    r10 = Column(Integer)  # пушка Грабина
     k10 = Column(Integer)
-    r11 = Column(Integer)  # kovorking
+    r11 = Column(Integer)  # коворкинг
     k11 = Column(Integer)
-    r12 = Column(Integer)  # LES antikafe
+    r12 = Column(Integer)  # антикафе Лес
     k12 = Column(Integer)
     r13 = Column(Integer)  # кванториум
     k13 = Column(Integer)
@@ -60,5 +60,5 @@ class CommandNames(Base):  # капитан создал - участники к
 class Agents(Base):  # кмк, агенты должны сами о себе заявить. Ну а хули
     __tablename__ = 'AGENTS'  # ну и у админа будет команда "айди номер этапа" чтобы дать челибосу админку
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(255))
+    agent_id = Column(String(255))
     stage_id = Column(String(255))  # от 1 до 14
