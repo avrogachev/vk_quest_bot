f1f0a668d3d32944a9184c4a517566e4efecebd4
Alexander Rogachev <rogachev-a-v@yandex.ru>
2019-09-10 19:44:01 +0300

    add description of table agents and commands DB

--- db_map.py
+++ db_map.py
@@ -10,11 +10,11 @@ Base = declarative_base()
 # 192168 |лупа|0-10   |8931|
 
 
-class General(Base):
-    __tablename__ = 'general'
+class General(Base):  # Когда капитан вводит название команды, генерируется строка этой таблицы и таблицы CommandNames
+    __tablename__ = 'GENERAL'
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(255))
-    name = Column(String(255))
+    captain_id = Column(String(255))
+    command_name = Column(String(255))
     r1 = Column(Integer)  # mark for solving - 0 to 15 ISZ
     k1 = Column(Integer)
     r2 = Column(Integer)  # Prokhodnaya RKK
@@ -39,12 +39,26 @@ class General(Base):
     k11 = Column(Integer)
     r12 = Column(Integer)  # LES antikafe
     k12 = Column(Integer)
-    r13 = Column(Integer)  # Kvantorium
+    r13 = Column(Integer)  # кванториум
     k13 = Column(Integer)
-    r14 = Column(Integer)  # Kraevedcheskiy muzey
+    r14 = Column(Integer)  # краеведческий музей
     k14 = Column(Integer)
-    p1 = Column(Integer)  #po 5 ballov za fotozadaniya
+    p1 = Column(Integer)  # по 5 баллов за фотозадания
     p2 = Column(Integer)
     p3 = Column(Integer)
     p4 = Column(Integer)
     p5 = Column(Integer)
+
+
+class CommandNames(Base):  # капитан создал - участники команды при вводе названия команды ищут по названию айди
+    __tablename__ = 'COMMANDNAMES'  # (нулл - варнинг) и, найдя его, делают новую запись айди команды - свой айди
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(255))
+    command_id = Column(String(255))
+
+
+class Agents(Base):  # кмк, агенты должны сами о себе заявить. Ну а хули
+    __tablename__ = 'AGENTS'  # ну и у админа будет команда "айди номер этапа" чтобы дать челибосу админку
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(255))
+    stage_id = Column(String(255))  # от 1 до 14
