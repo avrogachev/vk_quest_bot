4641d32f9fe536da0373085be6d524317989d967
Alexander Rogachev <rogachev-a-v@yandex.ru>
2019-09-12 09:15:35 +0300

    fix typo

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ class Commands(NamedRule):
     key = "commands"
 
     """
-    Own implementaion of commands rule.
+    Own implementation of commands rule.
     """
 
     def __init__(self, commands):
@@ -46,6 +46,7 @@ dp.setup_rule(Commands)  # bind
 
 
 USERS = {}  # schema - id: status
+
 
 class RegistrationMiddleware(BaseMiddleware):
     """
@@ -82,12 +83,14 @@ class IsAdmin(BaseRule):
         elif self.is_admin and status != "admin":
             return False
 
+
 storage = Storage()
 dp.storage = storage
 
 really_needed_counter = 0
 
 dp.storage.place("really_needed_counter", really_needed_counter)
+
 
 @dp.message_handler(payload={"command": 'start'})
 async def handle_start(message: types.Message, data: dict):
@@ -97,26 +100,38 @@ async def handle_start(message: types.Message, data: dict):
                         "Капитан должен зарегистрировать команду. "
                         "Как только он закончит, присоединяйтесь к нему и бегом в игру!",
                         keyboard=kb_choose.get_keyboard())
+
+
 @dp.message_handler(payload={"command": 'kb_choose_captain'})
 async def handle_choose_captain(message: types.Message, data: dict):
     await message.reply("Как называется твоя команда? Если ты не капитан, жми кнопку \"Назад\" ",
                         keyboard=kb_back_to_start.get_keyboard())
+
+
 @dp.message_handler(payload={"command": 'kb_choose_participant'})
 async def handle_choose_participant(message: types.Message, data: dict):
     await message.reply("Дождись, пока капитан зарегистрируется и скажи мне название твоей команды. "
                         "Если ты капитан, жми кнопку \"Назад\"", keyboard=kb_back_to_start.get_keyboard())
+
+
 @dp.message_handler(payload={"command": 'kb_back_to_start'})
 async def handle_back_to_start(message: types.Message, data: dict):
     await message.reply("В этот раз будь внимательнее:)", keyboard=kb_choose.get_keyboard())
+
+
 @dp.message_handler(payload={"command": 'tasks'})
 async def handle_tasks(message: types.Message, data: dict):
     c = dp.storage.get("really_needed_counter", 0)
     await message.reply("Тут будет список заданий.")
     dp.storage.update("really_needed_counter", c + 1)
     await message.answer(f"Hello! {c}")
+
+
 @dp.message_handler(payload={"command": 'help'})
 async def handle_help(message: types.Message, data: dict):
     await message.reply("Сейчас с вами свяжется агент из штаба, бот:(")
+
+
 @dp.message_handler(payload={"command": 'marks'})
 async def handle_marks(message: types.Message, data: dict):
     await message.reply("Тут будут баллы команды.")
